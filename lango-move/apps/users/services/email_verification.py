bb389af022fa06bde5bc2ebdd4abbbcde713a524
Gabriel <lasconi711@gmail.com>
2026-03-14 15:05:25 +0100

    more robust email_verification.py

--- lango-move/apps/users/services/email_verification.py
+++ lango-move/apps/users/services/email_verification.py
@@ -11,25 +11,42 @@ def send_verification_email(request, user):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
 
-    activation_url = request.build_absolute_uri(
-        reverse(
-            "activate-account",
-            kwargs={
-                "uidb64": uid,
-                "token": token,
-            },
-        )
+    activation_path = reverse(
+        "activate-account",
+        kwargs={
+            "uidb64": uid,
+            "token": token,
+        },
     )
+
+    site_url = getattr(settings, "SITE_URL", "").rstrip("/")
+
+    if request is not None:
+        try:
+            activation_url = request.build_absolute_uri(activation_path)
+        except Exception:
+            activation_url = f"{site_url}{activation_path}"
+    else:
+        activation_url = f"{site_url}{activation_path}"
 
     subject = "Activate your LangoMove account"
 
-    message = render_to_string(
-        "users/emails/verify_email.txt",
-        {
-            "user": user,
-            "activation_url": activation_url,
-        },
-    )
+    try:
+        message = render_to_string(
+            "users/emails/verify_email.txt",
+            {
+                "user": user,
+                "activation_url": activation_url,
+            },
+        )
+    except Exception:
+        message = (
+            f"Hello {user.username},\n\n"
+            f"Welcome to LangoMove.\n\n"
+            f"Please confirm your email address by clicking the link below:\n\n"
+            f"{activation_url}\n\n"
+            f"If you did not create this account, you can safely ignore this email.\n"
+        )
 
     send_mail(
         subject=subject,
@@ -39,8 +56,7 @@ def send_verification_email(request, user):
         fail_silently=False,
     )
 
-    if settings.DEBUG:
-        print("\n" + "=" * 80)
-        print("DEV ACTIVATION LINK:")
-        print(activation_url)
-        print("=" * 80 + "\n")
+    print("\n" + "=" * 80)
+    print("ACTIVATION LINK:")
+    print(activation_url)
+    print("=" * 80 + "\n")
